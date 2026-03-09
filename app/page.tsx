910fbde915e9f8db19b38d5d0c6d470ef27d5a11
alyssajsharpe <alyssajsharpe@gmail.com>
2026-03-09 15:20:34 -0700

    Updating file names

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-'use client'
+"use client"
 import Hero from "./components/hero/Hero";
 import Intro from "./components/intro/Intro";
-import ImageBanner from './components/banner/ImageBanner';
-import PhotoOfTheDay from "./components/photoOfTheDay/PhotoOfTheDay";
+import ImageBanner from "./components/banner/ImageBanner";
+import PhotoOfTheDay from "./components/PhotoOfTheDay/PhotoOfTheDay";
 import imageBanner1Image from "../public/images/space2.jpg";
 import imageBanner2Image from "../public/images/space.jpg";
-import Asteroid from './components/asteroid/Asteroid';
-import ImageCarousel from './components/carousel/ImageCarousel';
+import Asteroid from "./components/asteroid/Asteroid";
+import ImageCarousel from "./components/carousel/ImageCarousel";
 import Footer from "./components/footer/footer";
 
 export default function Home() {
